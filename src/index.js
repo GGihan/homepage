5108e62a96bb29eef5734e5c5ce5bd86a9cd0dae
ggihan <140435642+GGihan@users.noreply.github.com>
2026-03-14 21:01:07 +0100

    Add check for animation timeline support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import "./styles.css";
+
+
+// Checks for animation timeline support on current browser
+if (!CSS.supports('animation-timeline', 'scroll()')) {
+    const script = document.createElement('script');
+    script.src = 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+    document.head.appendChild(script);
+}
